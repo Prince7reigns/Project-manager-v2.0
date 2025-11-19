7f60b639aa16f8f7f53a09eb069d050f7e9ca4aa
Prince Yadav <prinsyadac@gmail.com>
2025-11-19 12:24:14 +0530

    refactor(App): integrate user context and enhance routing with Toaster notifications

--- Project-manager/frontend/src/App.jsx
+++ Project-manager/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import Login from './pages/auth/login.jsx';
 import SignUp from './pages/auth/signup.jsx';
@@ -11,11 +11,14 @@ import ManageUsers from './pages/admin/ManageUsers.jsx';
 import UserDashboard from './pages/User/UserDashboard';
 import MyTasks from './pages/User/MyTasks.jsx';
 import ViewTaksDetails from './pages/User/ViewTaksDetails.jsx';
+import UserProvider, { UserContext } from './context/userContext.jsx';
+import { Toaster } from 'react-hot-toast';
 
 
 const App = () => {
   return (
- 
+ <UserProvider>
+  <div>
     <Router>
       <Routes>
         {/* Public Routes - Accessible to everyone */}
@@ -30,13 +33,39 @@ const App = () => {
         </Route>
 
         <Route element={<PrivateRoute allowedRoles={['user']} />}>
-          <Route path="/user/UserDashboard" element={<UserDashboard />} />
+          <Route path="/user/dashboard" element={<UserDashboard />} />
           <Route path="/user/tasks" element={<MyTasks />} />
           <Route path="/user/tasks-details/:id" element={<ViewTaksDetails />} />
         </Route>
+
+       <Route path='/' element={<Root/>}/>
       </Routes>
     </Router>
+    </div>
+
+    <Toaster 
+    toastOptions={{
+      className:"",
+      style:{
+        fontSize:"13px",
+      }
+    }}
+    
+    />
+    </UserProvider>
   );
 };
 
 export default App;
+
+const Root = () =>{
+  const {user,loading} = useContext(UserContext)
+
+  if(loading) return <Outlet />
+
+  if(!user){
+    return <Navigate to="/login" />
+  }
+
+  return user?.role === "admin" ? <Navigate to="/admin/dashboard"/> : <Navigate to="user/dashboard"/>
+}
